37158afbbfa0ed1f96403e2cbc141d1cf1e70a46
EmilianoCBE <e.carrizales1305@gmail.com>
2025-10-06 14:55:20 -0600

    Corrección de paths

--- server.js
+++ server.js
@@ -1,38 +1,19 @@
-// const cors = require("cors");
-// const express = require("express");
-// const http = require("http");
-
-// const app = express();
-// const server = http.createServer(app);
-
-// const PORT = 3005;
-
-// const router = require("./routes.js");
-
-// app.use(cors({ origin: "http://localhost:5173" }));
-// app.use(express.json());
-
-// // 🔹 Servir la carpeta "uploads" para poder acceder a los PDFs
-// app.use("/uploads", express.static("uploads"));
-
-// app.use("/", router);
-
-// server.listen(PORT, () => {
-//   console.log(`Servidor escuchando en http://localhost:${PORT}`);
-// });
-
 const cors = require("cors");
 const express = require("express");
 const http = require("http");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const PORT = 3005;
 
-const router = require("./routes"); // <-- index.js importado
+const router = require("./routes");
 
 app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
+
+//Servir la carpeta "uploads" para poder acceder a los PDFs
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/", router);
 
